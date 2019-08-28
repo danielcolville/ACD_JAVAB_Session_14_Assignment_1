1d5a19d527a409c0baa889a0921976b9c9e7ceed
danielcolville <53836490+danielcolville@users.noreply.github.com>
2019-08-27 20:52:50 -0400

    Add files via upload

--- GetAndPost.java
+++ GetAndPost.java
@@ -32,27 +32,7 @@ public class GetAndPost extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		response.getWriter().append("<!DOCTYPE html>\r\n" + 
-				"<html>\r\n" + 
-				"<head>\r\n" + 
-				"<meta charset=\"ISO-8859-1\">\r\n" + 
-				"<title>Insert title here</title>\r\n" + 
-				"</head>\r\n" + 
-				"<body>\r\n" + 
-				"<form action=\"GetAndPost\" method=\"post\">\r\n" + 
-				"Enter your name: \r\n" + 
-				"<input type=\"text\" name=\"name\">\r\n" + 
-				"<input type=\"submit\" value=\"submit name\">\r\n" + 
-				"</form>\r\n" + 
-				"</body>\r\n" + 
-				"</html>");
-	}
-
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+		this.doPost(request,response);
 		String name=request.getParameter("name");
 		response.setContentType("text/html");
 		if(!name.equals("")) {
@@ -61,6 +41,28 @@ public class GetAndPost extends HttpServlet {
 		else {
 			response.getWriter().append("No name found");
 		}
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		
+		response.getWriter().append("<!DOCTYPE html>\r\n" + 
+				"<html>\r\n" + 
+				"<head>\r\n" + 
+				"<meta charset=\"ISO-8859-1\">\r\n" + 
+				"<title>Insert title here</title>\r\n" + 
+				"</head>\r\n" + 
+				"<body>\r\n" + 
+				"<form action=\"GetAndPost\" method=\"get\">\r\n" + 
+				"Enter your name: \r\n" + 
+				"<input type=\"text\" name=\"name\">\r\n" + 
+				"<input type=\"submit\" value=\"submit name\">\r\n" + 
+				"</form>\r\n" + 
+				"</body>\r\n" + 
+				"</html>");
 		
 	}
 
